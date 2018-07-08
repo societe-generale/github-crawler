7576f84a016e0880c5160e0195f82b4e18d8b7d7
Vincent FUCHS <vincent.fuchs@gmail.com>
2018-07-08 22:52:40 +0200

    changing variable name to detect when to make a release

--- release/deploy.sh
+++ release/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'makeRelease' ] ; then
+if [ "$TRAVIS_PULL_REQUEST_BRANCH" = 'makeRelease' ] ; then
     mvn release:prepare release:perform --settings release/mvnsettings.xml
 fi
