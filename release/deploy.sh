fdd4452bec23077e6c59c35d14eff8850423e5d4
vincent-fuchs <vincent.fuchs@gmail.com>
2018-07-09 13:14:32 +0200

    checking out master before doing the release

--- release/deploy.sh
+++ release/deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_PULL_REQUEST_BRANCH" = 'makeRelease' ] ; then
+    git checkout master
     mvn --batch-mode release:prepare release:perform --settings release/mvnsettings.xml
 fi
