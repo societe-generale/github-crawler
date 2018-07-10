4325cffabf6870ce3ba424202f36c3fd8404b404
Vincent FUCHS <vincent.fuchs@gmail.com>
2018-07-10 08:43:18 +0200

    adding before_deploy step and debug stuff

--- release/createReleaseProperties.sh
+++ release/createReleaseProperties.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-echo "scm:git:git@github.com:societe-generale/github-crawler.git" >> ../release.properties
-echo "scm.tag=$TRAVIS_TAG" >> ../release.properties
+echo "scm.url=scm:git:git@github.com:societe-generale/github-crawler.git" >> release.properties
+echo "scm.tag=$TRAVIS_TAG" >> release.properties
+
+echo "******release.properties content*******"
+cat release.properties
+
+echo "******directory content*******"
+pwd
+ls -al
