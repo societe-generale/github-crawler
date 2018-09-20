26fd9133a5c133d666e92d8677fa7519f6ebb3ba
Vincent Fuchs <vincent.fuchs@gmail.com>
2018-09-20 18:34:31 +0530

    new parser to count xml elements (#37)

---
+++ github-crawler-core/src/test/java/com/societegenerale/githubcrawler/parsers/CountXmlElementsParserTest.kt
@@ -0,0 +1,35 @@
+package com.societegenerale.githubcrawler.parsers
+
+import com.societegenerale.githubcrawler.IndicatorDefinition
+import com.societegenerale.githubcrawler.parsers.CountXmlElementsParser.Companion.XPATH_TO_MATCH
+import org.apache.commons.io.FileUtils
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import org.springframework.util.ResourceUtils
+
+class CountXmlElementsParserTest {
+
+    @Test
+    fun should_count_elements_matching_the_xpath() {
+
+        var nbModulesIndicator = IndicatorDefinition("nbProperties", CountXmlElementsParser.COUNT_XML_ELEMENTS_METHOD, mapOf(Pair(XPATH_TO_MATCH, "//*[local-name()='project']/*[local-name()='properties']/*")))
+        val countXmlElementsParser = CountXmlElementsParser()
+
+        val result = countXmlElementsParser.parseFileContentForIndicator(FileUtils.readFileToString(ResourceUtils.getFile("classpath:sample_pom.xml"), "UTF-8"), "", nbModulesIndicator)
+
+        assertThat(result.get("nbProperties")).isEqualTo("5")
+    }
+
+    @Test
+    fun should_report_0_if_no_match() {
+
+        var nbModulesIndicator = IndicatorDefinition("nbProperties", CountXmlElementsParser.COUNT_XML_ELEMENTS_METHOD, mapOf(Pair(XPATH_TO_MATCH, "//*[local-name()='project']/*[local-name()='modules']/*")))
+        val countXmlElementsParser = CountXmlElementsParser()
+
+        val result = countXmlElementsParser.parseFileContentForIndicator(FileUtils.readFileToString(ResourceUtils.getFile("classpath:sample_pom.xml"), "UTF-8"), "", nbModulesIndicator)
+
+        assertThat(result.get("nbProperties")).isEqualTo("0")
+    }
+
+
+}
