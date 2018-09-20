152e90c0a7e93bfd3a9903969b3b319f2e2214dd
vincent-fuchs <vincent.fuchs@gmail.com>
2018-09-20 16:47:26 +0530

    proper assertion in test

--- github-crawler-core/src/test/java/com/societegenerale/githubcrawler/output/HttpOutputTest.kt
+++ github-crawler-core/src/test/java/com/societegenerale/githubcrawler/output/HttpOutputTest.kt
@@ -18,61 +18,53 @@ import org.springframework.web.client.RestTemplate
 import java.util.*
 
 
-class HttpOutputTest{
+class HttpOutputTest {
 
     private val logger = LoggerFactory.getLogger(HttpOutput::class.java!!)
 
 
     @Test
-    fun shouldLogResponseBodyWhenErrorDuringPost(){
+    fun shouldLogResponseBodyWhenErrorDuringPost() {
         //Mock logging infra
         val root = LoggerFactory.getLogger(ch.qos.logback.classic.Logger.ROOT_LOGGER_NAME) as ch.qos.logback.classic.Logger
-        val mockAppender : Appender<ILoggingEvent> = mock()
+        val mockAppender: Appender<ILoggingEvent> = mock()
 
         `when`(mockAppender.name).thenReturn("MOCK")
         root.addAppender(mockAppender)
 
 
-        val mockRestTemplate : RestTemplate = mock()
+        val mockRestTemplate: RestTemplate = mock()
 
-        val httpOutput = HttpOutput("http://localhost",mockRestTemplate)
+        val httpOutput = HttpOutput("http://localhost", mockRestTemplate)
 
         `when`(mockRestTemplate.postForEntity(anyString(),
                                               any(OutputIndicator::class.java),
                                               eq(String::class.java)
                                              )
-             ).thenThrow(HttpClientErrorException(HttpStatus.BAD_REQUEST,"Bad Request details"))
+              ).thenThrow(HttpClientErrorException(HttpStatus.BAD_REQUEST, "Bad Request details"))
 
-
-        val dummyRepo=Repository(name = "repo1",
+        val dummyRepo = Repository(name = "repo1",
                 creationDate = Date(),
-                config= null,
+                config = null,
                 defaultBranch = "master",
                 fullName = "orgName/repoName1",
                 lastUpdateDate = Date(),
                 ownerTeam = null,
                 reason = null,
-                url="http://hello",
+                url = "http://hello",
                 branchesToParse = listOf(Branch("master"))
         )
 
         //ACTION
         httpOutput.output(dummyRepo)
 
-
         argumentCaptor<ILoggingEvent>().apply {
             verify(mockAppender).doAppend(capture())
 
-            assertThat(allValues).isNotEmpty
+            val matchingLogEvent = allValues.firstOrNull{ logEvent -> logEvent.formattedMessage.contains("Bad Request details") }
 
-//            val logMessage = firstValue as String
-//         //   val command = (firstValue as GenericCommandMessage<SaveSyndicationFacilityCommand>).payload
-//            assertThat(logMessage).isEqualTo("Bad Request details")
+            assertThat(matchingLogEvent ).`as`("can't find a log statement with expected message").isNotNull
         }
-
     }
-
-
-
 }
 
