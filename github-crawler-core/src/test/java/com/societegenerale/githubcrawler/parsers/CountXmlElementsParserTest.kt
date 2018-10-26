46b3ef4e3b7fb8c166e807089697ffbe82624d5e
Vincent Fuchs <vincent.fuchs@gmail.com>
2018-10-26 15:27:49 +0530

    catch exception internally in CountXmlElementsParser (#45)
    * updated changelog with latest changes
    
    * renaming variables
    
    * catching parsing exceptions in CountXmlElementsParser to avoid crashing the crawler

--- github-crawler-core/src/test/java/com/societegenerale/githubcrawler/parsers/CountXmlElementsParserTest.kt
+++ github-crawler-core/src/test/java/com/societegenerale/githubcrawler/parsers/CountXmlElementsParserTest.kt
@@ -9,13 +9,14 @@ import org.springframework.util.ResourceUtils
 
 class CountXmlElementsParserTest {
 
+    val countXmlElementsParser = CountXmlElementsParser()
+
     @Test
     fun should_count_elements_matching_the_xpath() {
 
-        var nbModulesIndicator = IndicatorDefinition("nbProperties", CountXmlElementsParser.COUNT_XML_ELEMENTS_METHOD, mapOf(Pair(XPATH_TO_MATCH, "//*[local-name()='project']/*[local-name()='properties']/*")))
-        val countXmlElementsParser = CountXmlElementsParser()
+        var nbPropertiesIndicator = IndicatorDefinition("nbProperties", CountXmlElementsParser.COUNT_XML_ELEMENTS_METHOD, mapOf(Pair(XPATH_TO_MATCH, "//*[local-name()='project']/*[local-name()='properties']/*")))
 
-        val result = countXmlElementsParser.parseFileContentForIndicator(FileUtils.readFileToString(ResourceUtils.getFile("classpath:sample_pom.xml"), "UTF-8"), "", nbModulesIndicator)
+        val result = countXmlElementsParser.parseFileContentForIndicator(FileUtils.readFileToString(ResourceUtils.getFile("classpath:sample_pom.xml"), "UTF-8"), "", nbPropertiesIndicator)
 
         assertThat(result.get("nbProperties")).isEqualTo("5")
     }
@@ -23,13 +24,21 @@ class CountXmlElementsParserTest {
     @Test
     fun should_report_0_if_no_match() {
 
-        var nbModulesIndicator = IndicatorDefinition("nbProperties", CountXmlElementsParser.COUNT_XML_ELEMENTS_METHOD, mapOf(Pair(XPATH_TO_MATCH, "//*[local-name()='project']/*[local-name()='modules']/*")))
-        val countXmlElementsParser = CountXmlElementsParser()
+        var nbModulesIndicator = IndicatorDefinition("nbModules", CountXmlElementsParser.COUNT_XML_ELEMENTS_METHOD, mapOf(Pair(XPATH_TO_MATCH, "//*[local-name()='project']/*[local-name()='modules']/*")))
 
         val result = countXmlElementsParser.parseFileContentForIndicator(FileUtils.readFileToString(ResourceUtils.getFile("classpath:sample_pom.xml"), "UTF-8"), "", nbModulesIndicator)
 
-        assertThat(result.get("nbProperties")).isEqualTo("0")
+        assertThat(result.get("nbModules")).isEqualTo("0")
     }
 
+    @Test
+    fun should_catch_parsing_exception_internally() {
+
+        var nbModulesIndicator = IndicatorDefinition("nbModules", CountXmlElementsParser.COUNT_XML_ELEMENTS_METHOD, mapOf(Pair(XPATH_TO_MATCH, "project/modules")))
+
+        val result = countXmlElementsParser.parseFileContentForIndicator(FileUtils.readFileToString(ResourceUtils.getFile("classpath:sample_pom_with_namespace_issue.xml"), "UTF-8"), "", nbModulesIndicator)
+
+        assertThat(result.get("nbModules")).isEqualTo("issue while parsing the file")
+    }
 
 }
