7412ff90c6b6b0592c925fd207100d72e5e1a303
Vincent Fuchs <vincent.fuchs@gmail.com>
2018-11-07 16:30:04 +0530

    enabling properties completion in the IDE (#51)

--- github-crawler-core/src/main/kotlin/com/societegenerale/githubcrawler/GitHubCrawlerProperties.kt
+++ github-crawler-core/src/main/kotlin/com/societegenerale/githubcrawler/GitHubCrawlerProperties.kt
@@ -1,9 +1,11 @@
 package com.societegenerale.githubcrawler
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 
 
 @ConfigurationProperties
+@EnableConfigurationProperties
 class GitHubCrawlerProperties(val searchesPerRepo: Map<String, SearchParam> = HashMap(),
                               val indicatorsToFetchByFile: Map<com.societegenerale.githubcrawler.FileToParse, List<IndicatorDefinition>> = HashMap(),
                               var repositoriesToExclude: List<String> = ArrayList(),
