263372919ef7f860775cfad17750cf04c5ee87c3
Vincent FUCHS <vincent.fuchs@gmail.com>
2018-11-20 21:37:32 +0530

    renaming "method" into "type", for config consistency purpose

--- github-crawler-core/src/main/kotlin/com/societegenerale/githubcrawler/IndicatorDefinition.kt
+++ github-crawler-core/src/main/kotlin/com/societegenerale/githubcrawler/IndicatorDefinition.kt
@@ -2,8 +2,8 @@ package com.societegenerale.githubcrawler
 
 //TODO check regularly on https://github.com/spring-projects/spring-boot/issues/8762 if it's solved.
 // config properties can't binded as immutable values, so we need to have a workaround, declaring all attributes as mutable
-// in previous version, name and method were lateinit, but in that case, it wasn't possible (or I didn't find) a way to set the values in unit tests.
+// in previous version, name and type were lateinit, but in that case, it wasn't possible (or I didn't find) a way to set the values in unit tests.
 // we should roll this back to immutable variables whenever Spring Boot will allow us to do it
 class IndicatorDefinition(var name: String="N/A",
-                          var method: String = "N/A",
+                          var type: String = "N/A",
                           var params: Map<String,String> = HashMap())
