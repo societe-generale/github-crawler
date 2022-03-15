3c9e878857ef639153449a91ba275c6584150aa8
Vincent Fuchs <vincent.fuchs@gefa.de>
2022-03-15 09:37:50 +0100

    adding type in gitHubConfig

--- github-crawler-core/src/main/kotlin/com/societegenerale/githubcrawler/GithubConfig.kt
+++ github-crawler-core/src/main/kotlin/com/societegenerale/githubcrawler/GithubConfig.kt
@@ -4,7 +4,12 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 
 
 @ConfigurationProperties("github-crawler.githubConfig")
-class GithubConfig(var apiUrl: String="",
+class GithubConfig(var type: SourceControlType = SourceControlType.GITHUB,
+                   var apiUrl: String="",
                    var oauthToken: String="",
                    var organizationName: String="",
                    var crawlUsersRepoInsteadOfOrgasRepos: Boolean=false)
+
+enum class SourceControlType {
+  GITLAB, GITHUB
+}
