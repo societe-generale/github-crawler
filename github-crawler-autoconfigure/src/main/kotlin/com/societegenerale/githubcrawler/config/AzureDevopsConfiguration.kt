24f0c060121a0b152191d4ed45dd04ab9886b77f
Vincent Fuchs <vincent.fuchs@gefa.de>
2022-03-21 19:34:29 +0100

    added on logging statement

--- github-crawler-autoconfigure/src/main/kotlin/com/societegenerale/githubcrawler/config/AzureDevopsConfiguration.kt
+++ github-crawler-autoconfigure/src/main/kotlin/com/societegenerale/githubcrawler/config/AzureDevopsConfiguration.kt
@@ -5,6 +5,7 @@ import com.societegenerale.githubcrawler.GitHubCrawlerProperties
 import com.societegenerale.githubcrawler.remote.RemoteAzureDevopsImpl
 import com.societegenerale.githubcrawler.remote.RemoteAzureDevopsImpl.Companion.AZURE_DEVOPS_URL
 import com.societegenerale.githubcrawler.remote.RemoteGitHub
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -12,6 +13,8 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 @ConditionalOnProperty(prefix = "github-crawler.githubConfig", name = ["type"], havingValue = "AZURE_DEVOPS")
 open class AzureDevopsConfiguration {
+
+    val log = LoggerFactory.getLogger(this.javaClass)
 
     @Bean
     open fun remoteAzureDevops(gitHubCrawlerProperties: GitHubCrawlerProperties): RemoteGitHub {
@@ -24,6 +27,8 @@ open class AzureDevopsConfiguration {
         else{
             targetUrl=gitHubCrawlerProperties.githubConfig.apiUrl
         }
+
+        log.info("URL for AzureDevops : $targetUrl")
 
         return RemoteAzureDevopsImpl(targetUrl,
                                      gitHubCrawlerProperties.githubConfig.organizationName,
