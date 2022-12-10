41ed7a0cb51c6ab98c4213210cb8171b9acee5fb
chandrasekharpanigrahy <chandrasekharpanigrahy@gmail.com>
2022-12-10 14:21:53 +0530

    mock files and server

--- github-crawler-autoconfigure/src/test/java/com/societegenerale/githubcrawler/mocks/RemoteServiceMock.java
+++ github-crawler-autoconfigure/src/test/java/com/societegenerale/githubcrawler/mocks/RemoteServiceMock.java
@@ -5,8 +5,9 @@ public interface RemoteServiceMock {
 
     static final int GITHUB_MOCK_PORT=9900;
 
-    static final int GITLAB_MOCK_PORT=8800;
+    static final int BITBUCKET_MOCK_PORT=7700;
 
+    static final int GITLAB_MOCK_PORT=8800;
 
     boolean start();
 
